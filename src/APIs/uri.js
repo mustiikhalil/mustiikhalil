8f36735dc7e527bf1de91868dceb742b459c6143
mustii <mustii@mmk.one>
2020-07-24 20:32:08 +0300

    Fixes URLs

--- src/APIs/uri.js
+++ src/APIs/uri.js
@@ -1,4 +1,4 @@
 var production = process.env.NODE_ENV === "production"
 
-const uri = production ? "https://salty-anchorage-12867.herokuapp.com" : "http://localhost:8000";
+const uri = production ? "https://safe-island-24152.herokuapp.com" : "http://localhost:8000";
 export default uri;
